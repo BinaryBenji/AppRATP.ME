c712199cd3c01ce433e3cf8507650530dd196f3c
BinaryBenji <ben639dx@gmail.com>
2017-03-13 22:26:00 +0100

    need to pass argues onClick

--- app/src/main/java/com/example/benji/myratp/MainActivity.java
+++ app/src/main/java/com/example/benji/myratp/MainActivity.java
@@ -8,6 +8,11 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String transport;
+    public String numero;
+    public String station;
+    public String sens;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +22,63 @@ public class MainActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), Actrerb.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        // Button RER
+
+        Button rer = (Button) findViewById(R.id.toggleButtonRER);
+        rer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                transport = "rers";
+            }
+        });
+
+
+        // Button METRO
+
+        Button metro = (Button) findViewById(R.id.toggleButtonMETRO);
+        metro.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                transport = "metros";
+            }
+        });
+
+        // Button BUS
+
+        Button bus = (Button) findViewById(R.id.toggleButtonBUS);
+        bus.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                transport = "bus";
+            }
+        });
+
+        // Sens Aller
+
+        Button sensall = (Button) findViewById(R.id.toggleButtonAller);
+        sensall.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                sens = "aller";
+            }
+        });
+
+        // Sens Retour
+
+        Button sensret = (Button) findViewById(R.id.toggleButtonRetour);
+        sensret.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                sens = "retour";
+            }
+        });
+
+        Button affi = (Button) findViewById(R.id.buttonChck);
+        affi.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+
+
+                Intent myIntent = new Intent(view.getContext(), activity_affiche.class);
                 startActivityForResult(myIntent, 0);
             }
         });
