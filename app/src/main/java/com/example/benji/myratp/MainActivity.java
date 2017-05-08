404add6c393ecfa376cd75ae9257f0a5470ca094
BinaryBenji <ben639dx@gmail.com>
2017-05-08 18:56:40 +0200

    smoother

--- app/src/main/java/com/example/benji/myratp/MainActivity.java
+++ app/src/main/java/com/example/benji/myratp/MainActivity.java
@@ -8,115 +8,252 @@ import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String transport;
-    public static String numero;
-    public static String station;
-    public static String sens;
-    public static String req;
-    //activity_affiche activity_affiche = new activity_affiche();
+    public static String req1;
+    public static String req2;
+    public static String req3;
+    public static String req4;
+    public static String req5;
+    public static String req6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        req1 = "https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/b/Robinson/A";
+        req2 = "https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/b/Cité_Universitaire/R";
+        req3 = "https://api-ratp.pierre-grimaud.fr/v3/schedules/tramways/3a/Cité_Universitaire/A";
+        req4 = "https://api-ratp.pierre-grimaud.fr/v3/schedules/tramways/3a/Porte_d'Italie/R";
+        req5 = "https://api-ratp.pierre-grimaud.fr/v3/schedules/metros/7/Porte_d'Italie/A";
+        req6 = "https://api-ratp.pierre-grimaud.fr/v3/schedules/metros/7/Pierre_et_Marie_Curie/R";
+
         super.onCreate(savedInstanceState);
-        //activity_affiche.Creat_activity_affiche(req);
         setContentView(R.layout.activity_main);
 
-        /*Button next = (Button) findViewById(R.id.buttonrerb);
-        next.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), Actrerb.class);
-                startActivityForResult(myIntent, 0);
-            }
-        });*/
-
-        // Button RER
-
-        Button rer = (Button) findViewById(R.id.toggleButtonRER);
-        rer.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                transport = "rers";
-            }
-        });
-
-
-        // Button METRO
-
-        Button metro = (Button) findViewById(R.id.toggleButtonMETRO);
-        metro.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                transport = "metros";
-            }
-        });
-
-        // Button BUS
-
-        Button bus = (Button) findViewById(R.id.toggleButtonBUS);
-        bus.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                transport = "bus";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, req1,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONObject res = jsonObj.getJSONObject("result");
+                            JSONArray sch = res.getJSONArray("schedules");
+                            String info = sch.getString(1);
+                            TextView rob1 = (TextView) findViewById(R.id.rob1);
+                            String[] parts = info.split(",");
+                            int i = 0;
+                            while (parts[i] != null) {
+                                if (parts[i].toLowerCase().contains("message".toLowerCase())) {
+                                    rob1.setText(parts[i].substring(11, (parts[i].length() - 17)));
+                                    break;
+                                }
+                                i++;
+                            }
+                        } catch (JSONException e) {
+                            System.out.print("No work");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView rob1 = (TextView) findViewById(R.id.rob1);
+                rob1.setText("That didn't work!");
             }
         });
+        queue.add(stringRequest);
 
-        // Sens Aller
 
-        Button sensall = (Button) findViewById(R.id.toggleButtonAller);
-        sensall.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                sens = "A";
+        queue = Volley.newRequestQueue(this);
+        stringRequest = new StringRequest(Request.Method.GET, req2,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONObject res = jsonObj.getJSONObject("result");
+                            JSONArray sch = res.getJSONArray("schedules");
+                            String info = sch.getString(1);
+                            TextView cit1 = (TextView) findViewById(R.id.cit1);
+                            String[] parts = info.split(",");
+                            int i = 0;
+                            while (parts[i] != null) {
+                                if (parts[i].toLowerCase().contains("message".toLowerCase())) {
+                                    cit1.setText(parts[i].substring(11, (parts[i].length() - 1)));
+                                    break;
+                                }
+                                i++;
+                            }
+                        } catch (JSONException e) {
+                            System.out.print("No work");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView cit1 = (TextView) findViewById(R.id.cit1);
+                cit1.setText("That didn't work!");
             }
         });
+        queue.add(stringRequest);
 
-        // Sens Retour
 
-        Button sensret = (Button) findViewById(R.id.toggleButtonRetour);
-        sensret.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                sens = "R";
+
+
+        queue = Volley.newRequestQueue(this);
+        stringRequest = new StringRequest(Request.Method.GET, req3,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONObject res = jsonObj.getJSONObject("result");
+                            JSONArray sch = res.getJSONArray("schedules");
+                            String info = sch.getString(1);
+                            TextView cit2 = (TextView) findViewById(R.id.cit2);
+                            String[] parts = info.split(",");
+                            int i = 0;
+                            while (parts[i] != null) {
+                                if (parts[i].toLowerCase().contains("message".toLowerCase())) {
+                                    cit2.setText(parts[i].substring(12, (parts[i].length() - 1)));
+                                    break;
+                                }
+                                i++;
+                            }
+                        } catch (JSONException e) {
+                            System.out.print("No work");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView cit2 = (TextView) findViewById(R.id.cit2);
+                cit2.setText("That didn't work!");
             }
         });
-
-        Button affi = (Button) findViewById(R.id.buttonChck);
-        affi.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
+        queue.add(stringRequest);
 
 
-                // NUM
 
-                EditText editnum  = (EditText)findViewById(R.id.txtNum);
-                numero =  editnum.getText().toString();
 
-                // STATION
-
-                EditText editstat  = (EditText)findViewById(R.id.txtStat);
-                station =  editstat.getText().toString();
-
-                // REQ
-
-                req = "https://api-ratp.pierre-grimaud.fr/v3/schedules/" +  transport + "/" + numero + "/" + station + "/" + sens;
-                //activity_affiche.Creat_activity_affiche(req);
-                System.out.println("main : " + req);
-
-                Intent myIntent = new Intent(view.getContext(), activity_affiche.class);
-                startActivityForResult(myIntent, 0);
+        queue = Volley.newRequestQueue(this);
+        stringRequest = new StringRequest(Request.Method.GET, req4,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONObject res = jsonObj.getJSONObject("result");
+                            JSONArray sch = res.getJSONArray("schedules");
+                            String info = sch.getString(1);
+                            TextView por1 = (TextView) findViewById(R.id.por1);
+                            String[] parts = info.split(",");
+                            int i = 0;
+                            while (parts[i] != null) {
+                                if (parts[i].toLowerCase().contains("message".toLowerCase())) {
+                                    por1.setText(parts[i].substring(12, (parts[i].length() - 1)));
+                                    break;
+                                }
+                                i++;
+                            }
+                        } catch (JSONException e) {
+                            System.out.print("No work");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView por1 = (TextView) findViewById(R.id.por1);
+                por1.setText("That didn't work!");
             }
-
         });
+        queue.add(stringRequest);
 
-   /* private void Affiche()
-    {
-        ArrayAdapter<activity_affiche> = new
-    }
 
-    private class TimeAdapter extends ArrayAdapter<activity_affiche>
-    {
-        public TimeAdapter(){}
-    }*/
+
+
+        queue = Volley.newRequestQueue(this);
+        stringRequest = new StringRequest(Request.Method.GET, req5,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONObject res = jsonObj.getJSONObject("result");
+                            JSONArray sch = res.getJSONArray("schedules");
+                            String info = sch.getString(1);
+                            TextView por2 = (TextView) findViewById(R.id.por2);
+                            String[] parts = info.split(",");
+                            int i = 0;
+                            while (parts[i] != null) {
+                                if (parts[i].toLowerCase().contains("message".toLowerCase())) {
+                                    por2.setText(parts[i].substring(12, (parts[i].length() - 1)));
+                                    break;
+                                }
+                                i++;
+                            }
+                        } catch (JSONException e) {
+                            System.out.print("No work");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView por2 = (TextView) findViewById(R.id.por2);
+                por2.setText("That didn't work!");
+            }
+        });
+        queue.add(stringRequest);
+
+
+        queue = Volley.newRequestQueue(this);
+        stringRequest = new StringRequest(Request.Method.GET, req6,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONObject res = jsonObj.getJSONObject("result");
+                            JSONArray sch = res.getJSONArray("schedules");
+                            String info = sch.getString(1);
+                            TextView pie1 = (TextView) findViewById(R.id.pie1);
+                            String[] parts = info.split(",");
+                            int i = 0;
+                            while (parts[i] != null) {
+                                if (parts[i].toLowerCase().contains("message".toLowerCase())) {
+                                    pie1.setText(parts[i].substring(12, (parts[i].length() - 1)));
+                                    break;
+                                }
+                                i++;
+                            }
+                        } catch (JSONException e) {
+                            System.out.print("No work");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView pie1 = (TextView) findViewById(R.id.pie1);
+                pie1.setText("That didn't work!");
+            }
+        });
+        queue.add(stringRequest);
+
 
     }
 }
--- app/src/main/java/com/example/benji/myratp/activity_affiche.java
+++ app/src/main/java/com/example/benji/myratp/activity_affiche.java
@@ -1,81 +1,81 @@
-package com.example.benji.myratp;
-
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class activity_affiche extends AppCompatActivity{
-
-    // req
-    //MainActivity main;
-
-    public String requ = MainActivity.req;
-    public String dep = MainActivity.station;
-
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_affiche);
-
-        Button next = (Button) findViewById(R.id.retour);
-        next.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                Intent intent = new Intent();
-                setResult(RESULT_OK, intent);
-                finish();
-            }
-        });
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, requ,
-                new Response.Listener<String>() { @Override
-                public void onResponse(String response) {
-                    try {
-                        JSONObject jsonObj = new JSONObject(response);
-                        JSONObject res = jsonObj.getJSONObject("result");
-                        JSONArray sch = res.getJSONArray("schedules");
-                        String info = sch.getString(1);
-                        TextView ttxt = (TextView) findViewById(R.id.txtTime);
-                        TextView trdep = (TextView) findViewById(R.id.txtDepart);
-
-                        trdep.setText(dep);
-                        String[] parts = info.split(",");
-                        int i = 0;
-                        while (parts[i] != null) {
-                            if(parts[i].toLowerCase().contains("message".toLowerCase())) {
-                                ttxt.setText(parts[i].substring(11, (parts[i].length()-1)));
-                                break;
-                            }
-                            i++;
-                        }
-                    }
-                    catch(JSONException e) {
-                        System.out.print("No work");
-                    }
-                }
-                }, new Response.ErrorListener() { @Override
-        public void onErrorResponse(VolleyError error) {
-            TextView ttxt = (TextView) findViewById(R.id.txtTime);
-            ttxt.setText("That didn't work!");
-        }
-
-        });
-        queue.add(stringRequest);
-    }
-}
+//package com.example.benji.myratp;
+//
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.StringRequest;
+//import com.android.volley.toolbox.Volley;
+//
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//public class activity_affiche extends AppCompatActivity{
+//
+//    // req
+//    //MainActivity main;
+//
+//    //public String requ = MainActivity.req;
+//    //public String dep = MainActivity.station;
+//
+//    protected void onCreate(Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_affiche);
+//
+//        Button next = (Button) findViewById(R.id.retour);
+//        next.setOnClickListener(new View.OnClickListener()
+//        {
+//            public void onClick(View view)
+//            {
+//                Intent intent = new Intent();
+//                setResult(RESULT_OK, intent);
+//                finish();
+//            }
+//        });
+//
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, requ,
+//                new Response.Listener<String>() { @Override
+//                public void onResponse(String response) {
+//                    try {
+//                        JSONObject jsonObj = new JSONObject(response);
+//                        JSONObject res = jsonObj.getJSONObject("result");
+//                        JSONArray sch = res.getJSONArray("schedules");
+//                        String info = sch.getString(1);
+//                        TextView ttxt = (TextView) findViewById(R.id.txtTime);
+//                        TextView trdep = (TextView) findViewById(R.id.txtDepart);
+//
+//                       // trdep.setText(dep);
+//                        String[] parts = info.split(",");
+//                        int i = 0;
+//                        while (parts[i] != null) {
+//                            if(parts[i].toLowerCase().contains("message".toLowerCase())) {
+//                                ttxt.setText(parts[i].substring(11, (parts[i].length()-1)));
+//                                break;
+//                            }
+//                            i++;
+//                        }
+//                    }
+//                    catch(JSONException e) {
+//                        System.out.print("No work");
+//                    }
+//                }
+//                }, new Response.ErrorListener() { @Override
+//        public void onErrorResponse(VolleyError error) {
+//            TextView ttxt = (TextView) findViewById(R.id.txtTime);
+//            ttxt.setText("That didn't work!");
+//        }
+//
+//        });
+//        queue.add(stringRequest);
+//    }
+//}
